0eb73c9524182ec0e09f9ae54e760480e63950a0
Louis Rey <rlhardrock@gmail.com>
2025-03-15 21:41:40 -0500

    new routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,26 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 // Rutas de autenticación eliminadas.
 
 export const routes: Routes = [
+  // Ruta para redirigir a dashboard
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, 
+  // Ruta para el dashboard
   { path: 'dashboard', component: DashboardComponent },
+
+  // Rutas para beneficios
+  { path: 'beneficio', loadComponent: () => import('./beneficios/beneficio-list/beneficio-list.component').then(m => m.BeneficioListComponent) }, // Carga el formulario
+  { path: 'beneficio/nuevo', loadComponent: () => import('./beneficios/beneficio-form/beneficio-form.component').then(m => m.BeneficioFormComponent) }, // Carga el formulario
+  { path: 'beneficio/editar/:id_remision', loadComponent: () => import('./beneficios/beneficio-form/beneficio-form.component').then(m => m.BeneficioFormComponent) }, // Carga el formulario
+
+  // Rutas para usuarios
+  { path: 'user', loadComponent: () => import('./users/user-list/user-list.component').then(m => m.UserListComponent) }, // Carga el formulario
+  { path: 'user/nuevo', loadComponent: () => import('./users/user-form/user-form.component').then(m => m.UserFormComponent) }, // Carga el formulario
+  { path: 'user/editar/:id', loadComponent: () => import('./users/user-form/user-form.component').then(m => m.UserFormComponent) }, // Carga el formulario
+
+  // Rutas para extras
+  /* { path: 'extra', loadComponent: () => import('./extras/extra-list.component').then(m => m.ExtraListComponent) }, // Carga el formulario
+  { path: 'extra/nuevo', loadComponent: () => import('./extras/extra-form.component').then(m => m.ExtraFormComponent) }, // Carga el formulario
+  { path: 'extra/editar/:id', loadComponent: () => import('./extras/extra-form.component').then(m => m.ExtraFormComponent) }, // Carga el formulario */
+
   { path: '**', redirectTo: '/dashboard' } 
 ];
 

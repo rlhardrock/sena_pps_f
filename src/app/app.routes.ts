9191e16668131145cab47b2c7ab3632545fe661d
Louis Rey <rlhardrock@gmail.com>
2025-03-15 19:57:15 -0500

    no login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
+// Rutas de autenticación eliminadas.
+
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, 
   { path: 'dashboard', component: DashboardComponent },

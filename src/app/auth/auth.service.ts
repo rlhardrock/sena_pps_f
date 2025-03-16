b1e627214a76d1a856383e117092345e64d93537
Louis Rey <rlhardrock@gmail.com>
2025-03-15 19:00:11 -0500

    login frontend  add POST

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -13,7 +13,10 @@ export class AuthService {
 
   login(email: string, password: string): Observable<any> {
     const data = { email, password }; // Crear el objeto con las credenciales
-    return this.http.post(`${this.apiUrl}/login`, data).pipe(
+    return this.http.post(`${this.apiUrl}/login`, data, {
+      headers: { 'Content-Type': 'application/json' }, // Asegurarse de que el tipo de contenido sea JSON
+      withCredentials: true, // Incluir credenciales si es necesario
+    }).pipe(
       catchError((error: HttpErrorResponse) => {
         console.error('Error en login:', error);
         return throwError(() => 'Error al iniciar sesión. Por favor, verifica tus credenciales e intenta de nuevo.');
